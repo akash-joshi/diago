de860783dcfa2555fcf1190833f6c479cd15285d
Akash Joshi <>
2020-07-08 21:22:52 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,63 @@
 import React, { useState } from "react";
 import ChatBot from "react-simple-chatbot";
+import { Video, File } from "react-feather";
+import styled from "styled-components";
 
 import initialMeet from "./initialMeet.json";
 
+const color = "black";
+
 const MainPage = (props) => {
-  return <section>Hello World !</section>;
+  const iconStyles = {
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginBottom: "0.2em",
+  };
+
+  const iconTextStyles = { fontSize: "0.7em", fontWeight: 500, color };
+
+  return (
+    <section>
+      Hello World !
+      <section
+        style={{
+          position: "fixed",
+          bottom: 0,
+          padding: "1em",
+          borderTop: "1px solid #e5e5e5",
+          width: "100%",
+          display: "flex",
+          justifyContent: "space-around",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            textAlign: "center",
+          }}
+        >
+          <File style={iconStyles} color={color} />
+          <div style={iconTextStyles}>Documents</div>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            textAlign: "center",
+          }}
+        >
+          <Video style={iconStyles} color={color} />
+          <div style={iconTextStyles}>Webinars</div>
+        </div>
+      </section>
+    </section>
+  );
 };
 
 function App() {
   const [initialData, setInitialData] = useState({});
-  const [screen, setScreen] = useState("InitialChatBot");
+  const [screen, setScreen] = useState("MainPage");
 
   const InitialChatBot = (props) => {
     const handleEnd = ({ steps, values }) => {
